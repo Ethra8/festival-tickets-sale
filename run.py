8546e4257d55312af087a831ac971d7de8622697
EdnaTMunill <80659091+Ethra8@users.noreply.github.com>
2023-12-02 15:40:03 +0100

    set python workspace

--- run.py
+++ run.py
@@ -1,6 +1,23 @@
 import pyfiglet 
-  
-logo = pyfiglet.figlet_format("ROCK FEST", font = "bubble" ) 
+import gspread #library first downloaded through terminal : pip3 install gspread google-auth
+from google.oauth2.service_account import Credentials #imports just specific Credentials function from library, no need to import complete library
+
+#SCOPE in a constant, in Python, constant variables are written in CAPITALS
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+#CREDS is another constant variable, takes creds from file
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('rock_fest')
+
+festival = "MUSIC FEST"
+font = "bubble"
+logo = pyfiglet.figlet_format(festival, font) 
 price_adult_one_day = 75 #value taken from spreadsheet
 price_adult_per_day = f"{price_adult_one_day} €"
 price_child_one_day = 45 #value taken from spreadsheet
